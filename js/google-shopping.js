f7801a1b9c32d1d417afbc512be0c04b16546138
shway250 <jackson_duhon@yahoo.com>
2016-07-20 17:53:28 -0700

    completed the 6 assignments for deliverable 3.

--- js/google-shopping.js
+++ js/google-shopping.js
@@ -1,5 +1,57 @@
 var data = require('../products.json');
 
-// Write your solutions below
+//////////////////////////NUMBER 1/////////////////////
+// var shoppingCount = 0;
 
-console.log(data.items);
+// for( var i = 0; i < data.items.length; i++){
+// 	var item = data.items[i];
+// 	if(item.kind === 'shopping#product'){
+// 		shoppingCount++;
+// 	}
+// }
+// console.log(shoppingCount);
+
+//console.log(data.items);
+
+//////////////////////////NUMBER 2//////////////////////////////
+
+// for(var i = 0; i<data.items.length; i++){
+// 	var item = data.items[i];
+// 	if(item.product.inventories[0].availability === 'backorder'){
+// 		console.log(item.product.title);
+// 	}
+// }
+
+
+////////////////////////NUMBER 3///////////////////////
+// for(var i = 0; i<data.items.length; i++){
+// 	var item = data.items[i];
+// 	if(item.product.images.length > 1){
+// 		console.log(item.product.title);
+// 	}
+// }
+
+////////////////////NUMBER 4 //////////////////////////
+
+// for(var i = 0; i<data.items.length; i++){
+// 	var item = data.items[i];
+// 	if(item.product.brand === "Canon"){
+// 		console.log(item.product.title);
+// 	}
+// }
+
+////////////////////NUMBER 5//////////////////////////
+
+// for(var i = 0; i<data.items.length; i++){
+// 	var item = data.items[i];
+// 	if(item.product.brand === "Canon" && item.product.author.name === "eBay"){
+// 		console.log(item.product.title);
+// 	}
+// }
+
+
+/////////////////////NUMBER 6//////////////////////
+for(var i = 0; i<data.items.length; i++){
+	var item = data.items[i];
+	console.log(item.product.brand, item.product.inventories[0].price, item.product.images[0].link );
+}
